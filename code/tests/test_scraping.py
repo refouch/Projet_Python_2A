11fccc716b2ebd7bd421cef9c860032baa66032f
remif <remi.foucherand@ensae.fr>
2024-11-26 13:40:11 +0000

    test ok

--- code/tests/test_scraping.py
+++ code/tests/test_scraping.py
@@ -2,14 +2,14 @@ import sys
 sys.path.append("code/")
 
 import unittest 
-import scraping 
+import scraping as s
 
 class Test_scraping(unittest.TestCase):
     def test_scraping_pays(self):
-        url = https://fr.wikipedia.org/wiki/L%C3%A9gislation_sur_le_cannabis
-        table = scrape_first_table(url)
-        data, header = table_to_list(table)
-        data_pays = make_pandas(data, header)
+        url = "https://fr.wikipedia.org/wiki/L%C3%A9gislation_sur_le_cannabis"
+        table = s.scrape_first_table(url)
+        data, header = s.table_to_list(table)
+        data_pays = s.make_pandas(data, header)
 
         self.assertEqual(data_pays.shape, (87,5))
         self.assertEqual(data_pays.iat[0,1], "Illégale")
