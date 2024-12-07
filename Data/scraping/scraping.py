8c440236c27de1f033d1d80869e16b0cb21401eb
remif <remi.foucherand@ensae.fr>
2024-12-07 15:18:02 +0000

    added function to scrape second table

--- Data/scraping/scraping.py
+++ Data/scraping/scraping.py
@@ -8,13 +8,25 @@ import pandas as pd
 url = "https://fr.wikipedia.org/wiki/L%C3%A9gislation_sur_le_cannabis"
 
 def scrape_first_table(url):
-    """ Fonction prenant en argument un lien wikipedia pour retourner le premier tableau de la page
+    """ Fonction prenant en argument un lien pour retourner le premier tableau de la page
         Arguments: url (str)
         Return: table (bs4 element)
     """
     request = rq.get(url)
     page = bs.BeautifulSoup(request.content, "html.parser")
     table = page.find("table")
+    return table
+
+def scrape_second_wikipedia_table(url):
+    """ Fonction prenant en argument un lien wikipedia pour retourner le second tableau de la page
+        Arguments: url (str)
+        Return: table (bs4 element)
+    """
+    request = rq.get(url)
+    page = bs.BeautifulSoup(request.content, "html.parser")
+    tables = page.find_all("table", {"class":"wikitable"})
+    table = tables[1]
+
     return table
 
 def table_to_list(table):
