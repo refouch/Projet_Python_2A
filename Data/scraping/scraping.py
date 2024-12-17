c7baaa6ce4f4ac12668ddcde03922d91d77f0373
qmallegol <quentin.mallegol@ensae.fr>
2024-12-17 15:11:05 +0000

    Catch up with main

--- Data/scraping/scraping.py
+++ Data/scraping/scraping.py
@@ -8,13 +8,25 @@ import pandas as pd
 url = "https://fr.wikipedia.org/wiki/L%C3%A9gislation_sur_le_cannabis"
 
 def scrape_first_table(url):
-    """ Fonction prenant en argument un lien wikipedia pour retourner le premier tableau de la page
+    """ Fonction prenant en argument un lien pour retourner le premier tableau de la page
         Arguments: url (str)
         Return: table (bs4 element)
     """
     request = rq.get(url)
-    page = bs.BeautifulSoup(request.content, "lxml")
+    page = bs.BeautifulSoup(request.content, "html.parser")
     table = page.find("table")
+    return table
+
+def scrape_second_wikipedia_table(url):
+    """ Fonction prenant en argument un lien wikipedia pour retourner le second tableau de la page
+        Arguments: url (str)
+        Return: table (bs4 element)
+    """
+    request = rq.get(url)
+    page = bs.BeautifulSoup(request.content, "html.parser")
+    tables = page.find_all("table", {"class":"wikitable"})
+    table = tables[1]
+
     return table
 
 def table_to_list(table):
@@ -44,7 +56,7 @@ def table_to_list(table):
 
     return data, header
 
-def make_pandas(data, header):
+def make_pandas(data, header = None):
     """Fonction prenant le tableau sous forme de liste pour le transformer en dataset pandas
     à l'aide d'un dictionnaire.
     Arguments: data, header (list)
@@ -55,8 +67,10 @@ def make_pandas(data, header):
         if len(row) > 0:
             dictionnaire[row[0]] = row[1:]
 
-    data_pays = pd.DataFrame.from_dict(dictionnaire, orient="index")
-    data_pays.columns = header[0][1:]
+        data_pays = pd.DataFrame.from_dict(dictionnaire, orient="index")
+
+    if header != None:
+        data_pays.columns = header[0][1:]
 
     return data_pays
 
